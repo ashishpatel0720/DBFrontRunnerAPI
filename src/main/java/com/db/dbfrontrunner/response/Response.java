eba259affe4c48f09a8765a5fc9c4715e97ba36b
Ashish Patel <ashishpatel0720@gmail.com>
2018-07-26 00:35:24 +0530

    LOGIN:User LogIn api done
    code(0): user not fond
    code(1): successfully logged in
    code(2): user found, password not match

--- src/main/java/com/db/dbfrontrunner/response/Response.java
+++ src/main/java/com/db/dbfrontrunner/response/Response.java
@@ -5,12 +5,14 @@ public class Response {
     private int code;
     private String title;
     private String description;
+    private Object data; // it will contain anything we want to send back
 
 
-    public Response(int code, String title, String description) {
+    public Response(int code, String title, String description,Object data) {
         this.code = code;
         this.title = title;
         this.description = description;
+        this.data=data;
     }
 
     @Override
@@ -20,6 +22,14 @@ public class Response {
                 ", title='" + title + '\'' +
                 ", description='" + description + '\'' +
                 '}';
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
     }
 
     public int getCode() {
--- src/main/java/com/db/dbfrontrunner/user/User.java
+++ src/main/java/com/db/dbfrontrunner/user/User.java
@@ -12,9 +12,7 @@ public class User {
 
     @Id
     private String id;
-
     private String name;
-
     private String password;
 
 	public String getId() {
@@ -39,5 +37,14 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id='" + id + '\'' +
+				", name='" + name + '\'' +
+				", password='" + password + '\'' +
+				'}';
 	}
 }
