2de0739de2104dedf03c12b450dac080fc8ac0b5
Goutham <gouthamkallempudi1996@gmail.com>
2018-07-31 10:57:53 +0530

    Database changed

--- src/main/java/com/db/dbfrontrunner/Controller/ordersBuySellController.java
+++ src/main/java/com/db/dbfrontrunner/Controller/ordersBuySellController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path="/users")
-public class ordersBuyController {
+public class ordersBuySellController {
 
     @Autowired
     ordersBuyRepository ordersBuy;
